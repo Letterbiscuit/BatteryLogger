eee89d17ff425e92f924ce1aadd9adba0ea3d974
Letterbiscuit <cmcculloch99@gmail.com>
2015-07-25 19:04:35 +0100

    Finished argument parsing, began writing parser for battery status files

--- src/batlog.c
+++ src/batlog.c
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
 
-void getValue(FILE *f);
+uint64_t getValue(FILE *f);//using uint64_t just in case someone has a really impressive battery
 
 
 
@@ -12,25 +13,61 @@ int main(int argc, char *argv[]){
 	FILE *outFile;	
 	FILE *fullFile;
 	FILE *chargeFile;
-	time_t interval;
-	time_t length;
+	time_t interval = 0;
+	time_t length = 0;
 	time_t start;
 	time_t now;
 	time_t elapsed;
 	time_t end;
-	struct timespec delay; 
-
+	struct timespec delay;
 	if (argc != 4){
 		printf("Usage: %s output interval length\n", argv[0]);
 		return 1;
 	}
-	interval = strtol(argv[2]);
-	length = strtol(argv[3]);
+	interval = strtol(argv[2], NULL, 10);
+	length = strtol(argv[3], NULL, 10);
+	if(errno || interval > length || interval == 0 || length == 0){
+	//Ensure that the numeric args were ok
+		puts("Invalid arguments");
+		puts("Note: length must be greater than interval");
+	return 1;
+	}
+	
 	now = time(NULL);
 	start = now;
 	end = start + interval;
+	delay.tv_nsec = 0;//No values less than a second to be waited
+	delay.tv_sec = interval;
+
+	outFile = fopen(argv[1], "w");
+	if(!outFile){
+		perror("Invalid output file");
+		return 1;
+	}
+	fullFile = fopen("/sys/class/power_supply/BAT0/energy_full", "r");
+	if(!fullFile){
+		perror("Error opening /sys/class/power_supply/BAT0/energy_full");
+		return 1;
+	}
+	getValue(fullFile);
+	fclose(fullFile);
 
 
 	return 0;
 }
 
+
+uint64_t getValue(FILE *f){
+	uint64_t value;
+	char current = fgetc(f);
+	char *readStr = calloc(1, 1);//One for \0
+	memset(readStr, '\0', 1);
+	while(current != EOF){
+		readStr = realloc(readStr, strlen(readStr) + 1);
+		strncat(readStr, &current, 1);
+		current = fgetc(f);
+	}
+
+	free(readStr);
+	return value;
+}
